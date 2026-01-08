c5ceef376e3177dfcfd55f9b2fb7dc190fdc06bc
Patrick de Almeida Oliveira Coelho <pcoelho18ao@gmail.com>
2026-01-08 14:32:58 -0300

    Delete mmeta_consultancy_system directory

--- mmeta_consultancy_system/Sistema_de_Consultoria_da_Mmeta/Sistema/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import listar_contratos
-
-urlpatterns = [
-    path('contratos/', listar_contratos),
-]
--- mmeta_consultancy_system/Sistema_de_Consultoria_da_Mmeta/Sistema/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Contrato
-
-def listar_contratos(request):
-    contratos = Contrato.objects.all()
-    return render(request, 'contratos.html', {'contratos': contratos})
