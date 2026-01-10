80605f692e3d5c7454a978fcf7b34a81b072159c
Adrielly Souza <adriellykerolyn@gmail.com>
2026-01-09 22:21:25 -0300

    Altera referência de Mmeta para Meta

--- Sistema_de_Consultoria_da_Meta/Sistema_de_Consultoria_da_Meta/asgi.py
+++ Sistema_de_Consultoria_da_Meta/Sistema_de_Consultoria_da_Meta/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Sistema_de_Consultoria_da_Mmeta project.
+ASGI config for Sistema_de_Consultoria_da_Meta project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sistema_de_Consultoria_da_Mmeta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sistema_de_Consultoria_da_Meta.settings')
 
 application = get_asgi_application()
+
