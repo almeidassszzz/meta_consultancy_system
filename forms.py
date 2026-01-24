5f17072d2958ca4e05f4f49014780ec4f4f026f3
Lettícia Sabino <145988893+letticiasabino@users.noreply.github.com>
2026-01-24 15:02:49 -0300

    Atualizando estilização
    Adicionei também  a criação automatica de id de contrato para evitar duplicidade.

---
+++ forms.py
@@ -0,0 +1,52 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Cliente, Servico, Contrato 
+
+
+class ClienteForm(forms.ModelForm): 
+    class Meta: 
+        model = Cliente 
+        fields = ['nome', 'cnpj'] 
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Ex: Nome Exemplo'}),
+            'cnpj': forms.NumberInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 15501550155015'}),
+        }
+
+
+class ServicoForm(forms.ModelForm): 
+    class Meta: 
+        model = Servico
+        fields = ['nome', 'preco_base'] 
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Ex: Consultoria Exemplo'}),
+            'preco_base': forms.NumberInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 1500.50'}),
+        }
+
+
+class ContratoForm(forms.ModelForm): 
+    class Meta: 
+        model = Contrato
+        fields = ['codigo', 'cliente', 'servico', 'valor_negociado', 'data_inicio', 'data_fim'] 
+        widgets = {
+            'codigo': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 01'}),
+            'cliente': forms.Select(attrs={'class': 'form-input'}),
+            'servico': forms.Select(attrs={'class': 'form-input'}),
+            'valor_negociado': forms.NumberInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 1500.50'}),
+            'data_inicio': forms.DateInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 07/01/2026'}),
+            'data_fim': forms.DateInput(attrs={'class': 'form-input', 'placeholder': 'Ex: 30/01/2026'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cliente'].empty_label = "Selecione um cliente"
+        self.fields['servico'].empty_label = "Selecione um serviço"
+
+
+# Formulário de cadastro de usuário
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Insira um email válido para prosseguir.')
+    
+    class Meta:  
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
---
+++ models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from datetime import date
+from django.core.exceptions import ValidationError
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length = 255, unique = True)
+    cnpj = models.CharField(max_length = 14, unique = True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    nome = models.CharField(max_length = 70, unique = True)
+    preco_base = models.DecimalField(max_digits = 10, decimal_places = 2)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contrato(models.Model):
+    codigo = models.CharField(max_length = 20, unique = True)
+    cliente = models.ForeignKey(Cliente, on_delete = models.PROTECT)
+    servico = models.ForeignKey(Servico, on_delete = models.PROTECT)
+    valor_negociado = models.DecimalField(max_digits = 10, decimal_places = 2)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+
+    def limpando_data(self):
+        data_limpa = super().clean()
+        data_inicio = data_limpa.get('data_inicio')
+        data_fim = data_limpa.get('data_fim')
+        
+        if data_inicio < data_fim:
+            raise ValidationError({
+                'data_inicio': 'A data final não pode ser anterior a data inicial.'
+            })
+        return data_limpa
+    
+    def status(self):
+        return "VIGENTE" if date.today() <= self.data_fim else "FINALIZADO"
+
+    def __str__(self):
+        return self.codigo
+
